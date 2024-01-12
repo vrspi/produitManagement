741ea09851d82d6751c4f9273f95c7c45db1068b
vrspi <53494644+vrspi@users.noreply.github.com>
2024-01-12 08:24:18 +0000

    add function add et aussi les test unit

--- gestion_produit/src/test/java/com/khairayoub/ProduitServiceTest.java
+++ gestion_produit/src/test/java/com/khairayoub/ProduitServiceTest.java
@@ -1,5 +1,23 @@
 package com.khairayoub;
 
+import com.khairayoub.Service.ProduitService;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class ProduitServiceTest {
-    
+
+    private ProduitService produitService;
+
+    @Before
+    public void setUp() {
+        produitService = new ProduitService();
+    }
+
+    @Test
+    public void testAjouterProduit() {
+        Produit produit = new Produit(1L, "Produit Test", 100.0, 10);
+        produitService.ajouterProduit(produit);
+        assertEquals(produit, produitService.produits.get(1L));
+    }
 }
